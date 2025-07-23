474f1f3c6239c7696c187d0eef1250902031da82
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2025-07-23 16:10:18 +0200

    wedding party

--- lib/features/wedding_party/wedding_party.dart
+++ lib/features/wedding_party/wedding_party.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:jaylin_wedding/features/wedding_party/person.dart';
 
 class WeddingPartyPage extends StatelessWidget {
   const WeddingPartyPage({super.key});
@@ -53,7 +54,54 @@ class WeddingPartyPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            const SizedBox(height: 40),
+            Text('Wedding Party', style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 25),
+              child: Text(
+                'Meet our friends and family who are walking down the aisle with us',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text('Her Sidekicks', style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: CircleAvatar(radius: 50, child: Icon(Icons.person)),
+                ),
+                Text('Maleah', style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        const TextSpan(text: 'She '),
+                        TextSpan(
+                          text: 'thinks',
+                          style: const TextStyle(decoration: TextDecoration.lineThrough),
+                        ),
+                        const TextSpan(text: ' knows she is a know it all'),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            Person(name: 'Aeryn', imagePath: '', description: 'aka Stickies, famous for her stickiness'),
+            Person(name: 'Jenna', imagePath: '', description: 'This is an example of a description'),
+            const SizedBox(height: 20),
+            Text('His Sidekicks', style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
+            const SizedBox(height: 10),
+            Person(name: 'Matthew', imagePath: '', description: 'This is an example of a description'),
+          ],
         ),
       ),
     );
